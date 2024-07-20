2da932173599f2e76286c115264852f052d50b4f
A SRINU <adondasrinu4@gmail.com>
2024-07-20 13:53:26 +0530

    first pro

---
+++ README.md
@@ -0,0 +1,1 @@
+# welcome to my would vaisree
---
+++ s.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from React"
+);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
